939990d0eb3efe74a4d4c9be2db38fe7b1889397
patrickmj <patrickmjchnm@gmail.com>
2014-11-02 11:52:35 -0500

    Handle "Don't show element set headers" situation

--- config_form.php
+++ config_form.php
@@ -5,7 +5,7 @@ vocabulary with the <a href="http://omeka.org/add-ons/plugins/simple-vocab/">Sim
 $elTable = get_db()->getTable('Element');
 $data = $elTable->findPairsForSelectForm();
 $linkedElements = unserialize(get_option('search_by_metadata_elements'));
-
+$view = get_view();
 $values = array();
 if(is_array($linkedElements)) {
     foreach($linkedElements as $elSet=>$elements) {
@@ -16,27 +16,17 @@ if(is_array($linkedElements)) {
     }
 }
 
-
-foreach($data as $elSet=>$options) {
-    echo "<div class='field'>";
-    echo "<h2>$elSet</h2>";
-    echo get_view()->formMultiCheckbox('element_sets', $values, null, $options, '');
-    echo "</div>";
+if (get_option('show_element_set_headings') ) {
+    foreach($data as $elSet=>$options) {
+        echo "<div class='field'>";
+        echo "<h2>$elSet</h2>";
+        echo $view->formMultiCheckbox('element_sets', $values, null, $options, '');
+        echo "</div>";
+    }
+} else {
+    foreach($data as $elSet=>$options) {
+        echo "<div class='field'>";
+        echo $view->formMultiCheckbox('element_sets', $values, null, $options, '');
+        echo "</div>";
+    }
 }
-
-
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
