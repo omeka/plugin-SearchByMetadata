0fca584427461a6ee8e88890a9d4135c4f3e0b47
John Flatness <john@zerocrates.org>
2014-11-18 12:41:59 -0500

    Tweak wording.

--- config_form.php
+++ config_form.php
@@ -1,5 +1,5 @@
-<p>Check the metadata fields that you want to make search links. Recommended best practice is to use a controlled
-vocabulary with the <a href="http://omeka.org/add-ons/plugins/simple-vocab/">Simple Vocab</a> plugin on the fields that will be made into make links.</a>
+<p>Check the metadata fields that you want to make into search links. Recommended best practice is to use a controlled
+vocabulary with the <a href="http://omeka.org/add-ons/plugins/simple-vocab/">Simple Vocab</a> plugin on the fields that will be made into links.</p>
 
 <?php
 $elTable = get_db()->getTable('Element');
